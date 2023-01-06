dc2ad4415973ef51dd3e26473cc57cf294ada796
Minhaj Islam <minhajislam674@gmail.com>
2023-01-06 11:56:06 +0100

    Implemented login and signup features

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -2,12 +2,26 @@ import React from "react";
 import { useState, useEffect  } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
+import { LoginView } from "../login-view/login-view";
+import { SignupView } from "../signup-view/signup-view";
 
 export const MainView = () => {
+    const storedUser = JSON.parse(localStorage.getItem("user"));
+    const storedToken = localStorage.getItem("token");
     const [movies, setMovies] = useState([]);
+    const [selectedMovie, setSelectedMovie] = useState(null);
+    const [user, setUser] = useState(storedUser? storedUser: null);
+    const [token, setToken] = useState(storedToken? storedToken: null);
+
 
     useEffect(() => {
-        fetch("https://myflix-movies.onrender.com/movies")
+        if(!token) {
+            return;
+        }
+
+        fetch("https://myflix-movies.onrender.com/movies", {
+            headers: {Authorization: `Bearer ${token}`}
+        })
           .then((response) => response.json())
           .then((data) => {
             const moviesFromApi = data.map((doc) => {
@@ -23,26 +37,47 @@ export const MainView = () => {
     
             setMovies(moviesFromApi);
           });
-      }, []);
+      }, [token]);
 
-    const [selectedMovie, setSelectedMovie] = useState(null);
 
+    if (!user) {
+        return (
+            <>
+                <LoginView
+                    onLoggedIn = {(user, token) => {
+                        setUser(user);
+                        setToken(token);
+                }}/>
+
+                <SignupView />
+            </>
+        );
+    }   
+
+    
     if (selectedMovie) {
-        return <MovieView
+        return (
+            <>
+                <MovieView
                     movieData={selectedMovie}
                     onBackClick = {()=> {
                         setSelectedMovie(null)
                     }}
                 />
+                <hr/>
+                <h2> Similar movies</h2>
+            </>
+
+        )
     }
 
-
     if (movies.length ===0) {
-        return <div> The list is empty!</div>
+        return <div>Loading movies...</div>
     }
 
     return (
         <div>
+            <button onClick={() => { setUser(null); setToken(null) }}>Logout</button>
             {movies.map((movie)=> {
                 return <MovieCard
                             key={movie.id}
