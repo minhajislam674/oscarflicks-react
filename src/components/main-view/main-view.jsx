e1849676b1dd499239f8c6629f50c795df1d046a
Minhaj Islam <minhajislam674@gmail.com>
2023-04-06 22:10:02 +0200

    Added a spinner component to show while loading movie data

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { useState, useEffect  } from "react";
+import { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 import { LoginView } from "../login-view/login-view";
@@ -10,201 +10,160 @@ import { DirectorView } from "../director-view/director-view";
 import { GenreView } from "../genre-view/genre-view";
 import { NavBar } from "../navigation-bar/navigation-bar";
 import { Col, Row, Container } from "react-bootstrap";
-import { BrowserRouter, Routes, Route, Navigate  } from 'react-router-dom';
-import './main-view.scss'
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import SpinnerComponent from "../spinner/spinner-component";
 
-
+import "./main-view.scss";
 
 export const MainView = () => {
-    const storedUser = JSON.parse(localStorage.getItem("user"));
-    const storedToken = localStorage.getItem("token");
-    const [movies, setMovies] = useState([]);
-    const [user, setUser] = useState(storedUser? storedUser: null);
-    const [token, setToken] = useState(storedToken? storedToken: null);
-    const [filteredMovies, setFilteredMovies] = useState(movies);
-    const [searchQuery, setSearchQuery] = useState('');
+  const storedUser = JSON.parse(localStorage.getItem("user"));
+  const storedToken = localStorage.getItem("token");
+  const [movies, setMovies] = useState([]);
+  const [user, setUser] = useState(storedUser ? storedUser : null);
+  const [token, setToken] = useState(storedToken ? storedToken : null);
+  const [filteredMovies, setFilteredMovies] = useState(movies);
+  const [searchQuery, setSearchQuery] = useState("");
 
+  useEffect(() => {
+    if (!token) {
+      return;
+    }
 
+    fetch("https://myflix-movies.onrender.com/movies", {
+      headers: { Authorization: `Bearer ${token}` },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setMovies(data);
+        setFilteredMovies(data);
+      });
+  }, [token]);
 
-    useEffect(() => {
-        if(!token) {
-            return;
-        }
-
-        fetch("https://myflix-movies.onrender.com/movies", {
-            headers: {Authorization: `Bearer ${token}`}
-        })
-          .then((response) => response.json())
-          .then((data) => {
-    
-            setMovies(data);
-            setFilteredMovies(data);
-          });
-      }, [token]);
-
-
-
-    return (
-    
+  return (
     <>
+      <BrowserRouter>
+        <NavBar user={user} />
 
-    <BrowserRouter>
-    <NavBar
-        user={user}
-    />
-
-
-    <Routes>
-        <Route
+        <Routes>
+          <Route
             path="/login"
-            element = {
-                <>
-                    { user ? (
-                        <Navigate to="/" />
-                    ) : (
-                        <LoginView
-                        onLoggedIn={(user, token) => {
-                            setUser(user);
-                            setToken(token);
-                        }}
-                    />
-                    )}
-                </>
-            } 
-        />
-
-        <Route
-            path="/register"
-            element = {
-                <>
-                    {
-                        <SignupView/>
-                    }
-                </>
+            element={
+              <>
+                {user ? (
+                  <Navigate to="/" />
+                ) : (
+                  <LoginView
+                    onLoggedIn={(user, token) => {
+                      setUser(user);
+                      setToken(token);
+                    }}
+                  />
+                )}
+              </>
             }
-        />
-        <Route
+          />
+
+          <Route path="/register" element={<>{<SignupView />}</>} />
+          <Route
             path="/movies/:movieId"
-            element = {
-                <>
-                    { !user ? (
-                        <Navigate to="/" />
-                    ) : (
-                        <MovieView
-                            movieData={movies}
-                        />
-                    )}
-                </>
-            } 
-        />
-
-        <Route
-            path="/directors/:directorName"
-            element = {
-                <>
-                    { !user ? (
-                        <Navigate to="/" />
-                    ) : movies.length ===0 ? (
-                        <div>Loading director information...</div>
-                    ) : (
-                        <DirectorView
-                            movieData={movies}
-
-                        />
-                    )}
-                </>
-            } 
-        />
-
-        <Route
-            path="/genre/:genreName"
-            element = {
-                <>
-                    { !user ? (
-                        <Navigate to="/" />
-                    ) : movies.length ===0 ? (
-                        <div>Loading genre information...</div>
-                    ) : (
-                        <GenreView
-                            movieData={movies}
-
-                        />
-                    )}
-                </>
-            } 
-        />
-
-        <Route
-            path="/users/:username"
-            element = {
-                <>
-                    { !user ? (
-                        <Navigate to="/" />
-                    ) : (
-                        <ProfileView
-                            movieData = {movies}
-                    />
-                    )}
-                </>
-            } 
-        />
-
-
-        <Route
-            path="/"
-            element = {
-                <>
-                    { !user ? ( 
-                        <Navigate to="/login" replace />
-                    ) : movies.length === 0 ? (
-                        <div>Loading movies...</div>
-                    ) : (
-                        
-                        <Container className="home-container" >
-                            <div>
-                                <input
-                                    className="search-bar"
-                                    type="search"
-                                    value={searchQuery}
-                                    placeholder="Search movies..."
-                                    onChange={(e) => {
-                                        const filteredArray =  movies.filter((movie)=>{
-                                            return movie.Title.toLowerCase().includes(e.target.value.toLowerCase());
-                                        })
-                                        setFilteredMovies(filteredArray);
-                                        setSearchQuery(e.target.value);
-                                        console.log(filteredArray);
-                                    }}
-                                >
-                                </input>
-
-                            </div>
-                            <Row className="justify-content-md-left" mb={10}>
-                            {filteredMovies.map((movie) => (
-                                <Col
-                                    className="mb-3 mt-3"
-                                    key={movie._id}
-                                    sm={12}
-                                    md={6}
-                                    lg={3}
-                                >
-                                    <MovieCard
-                                        movieData={movie}
-                                    />
-                                </Col>
-                        ))}
-                            </Row>
-                        </Container>
-                    )}
-                </>
+            element={
+              <>
+                {!user ? <Navigate to="/" /> : <MovieView movieData={movies} />}
+              </>
             }
-        />
+          />
 
-    </Routes>
-    </BrowserRouter>
-        
-        </>
-    
-);
+          <Route
+            path="/directors/:directorName"
+            element={
+              <>
+                {!user ? (
+                  <Navigate to="/" />
+                ) : movies.length === 0 ? (
+                  <SpinnerComponent />
+                ) : (
+                  <DirectorView movieData={movies} />
+                )}
+              </>
+            }
+          />
+
+          <Route
+            path="/genre/:genreName"
+            element={
+              <>
+                {!user ? (
+                  <Navigate to="/" />
+                ) : movies.length === 0 ? (
+                  <SpinnerComponent />
+                ) : (
+                  <GenreView movieData={movies} />
+                )}
+              </>
+            }
+          />
+
+          <Route
+            path="/users/:username"
+            element={
+              <>
+                {!user ? (
+                  <Navigate to="/" />
+                ) : (
+                  <ProfileView movieData={movies} />
+                )}
+              </>
+            }
+          />
+
+          <Route
+            path="/"
+            element={
+              <>
+                {!user ? (
+                  <Navigate to="/login" replace />
+                ) : movies.length === 0 ? (
+                  <SpinnerComponent />
+                ) : (
+                  <Container className="home-container">
+                    <div>
+                      <input
+                        className="search-bar"
+                        type="search"
+                        value={searchQuery}
+                        placeholder="Search movies..."
+                        onChange={(e) => {
+                          const filteredArray = movies.filter((movie) => {
+                            return movie.Title.toLowerCase().includes(
+                              e.target.value.toLowerCase()
+                            );
+                          });
+                          setFilteredMovies(filteredArray);
+                          setSearchQuery(e.target.value);
+                          console.log(filteredArray);
+                        }}
+                      ></input>
+                    </div>
+                    <Row className="justify-content-md-left" mb={10}>
+                      {filteredMovies.map((movie) => (
+                        <Col
+                          className="mb-3 mt-3"
+                          key={movie._id}
+                          sm={12}
+                          md={6}
+                          lg={3}
+                        >
+                          <MovieCard movieData={movie} />
+                        </Col>
+                      ))}
+                    </Row>
+                  </Container>
+                )}
+              </>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 };
-
--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -3,132 +3,172 @@ import { useNavigate, useParams } from "react-router";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import { Button, Card, CardImg, Row, Col, Container } from "react-bootstrap";
-import { toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import '../../assets/favorite-icon.png';
-import '../../assets/favorite-icon-clicked.png';
-import './movie-view.scss'
+import SpinnerComponent from "../spinner/spinner-component";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "../../assets/favorite-icon.png";
+import "../../assets/favorite-icon-clicked.png";
+import "./movie-view.scss";
 
+export const MovieView = ({ movieData }) => {
+  const { movieId } = useParams();
+  const movie = movieData.find((m) => m._id === movieId);
+  const favorited = require("../../assets/favorite-icon-clicked.png");
+  const notFavorited = require("../../assets/favorite-icon.png");
+  const [favorite, setFavorite] = useState(false);
 
+  const token = localStorage.getItem("token");
+  const navigate = useNavigate();
 
-export const MovieView = ({movieData}) => {
-    const {movieId} = useParams();
-    const movie = movieData.find((m) => m._id === movieId);
-    const favorited = require('../../assets/favorite-icon-clicked.png');
-    const notFavorited = require('../../assets/favorite-icon.png');
-    const [favorite, setFavorite] = useState(false);
-  
-    const token = localStorage.getItem("token");
-    const navigate = useNavigate();
-
-    const addToFavoriteSuccess = () => toast.info("Added to favorites!", {
+  const addToFavoriteSuccess = () =>
+    toast.info("Added to favorites!", {
       position: "top-right",
       autoClose: 2000,
       hideProgressBar: true,
       closeOnClick: true,
-      theme: "light"
-  });
+      theme: "light",
+    });
 
-    const removeFromFavoriteSuccess = () => toast.info("Removed from favorites!", {
+  const removeFromFavoriteSuccess = () =>
+    toast.info("Removed from favorites!", {
       position: "top-right",
       autoClose: 2000,
       hideProgressBar: true,
       closeOnClick: true,
-      theme: "light"
-  });
+      theme: "light",
+    });
 
-    
-
-
-
-    const handleFavorite = async () => {
-        const user = JSON.parse(localStorage.getItem("user"));
-        const response = await fetch("https://myflix-movies.onrender.com/users/"+user.Username, {
-                method: "GET",
-                headers: {
-                  Authorization: `Bearer ${token}`,
-                  "Content-Type": "application/json"
-                }
-              })
-              const data = await response.json();
-              const favoriteMovies = data.FavoriteMovies;
-              console.log(favoriteMovies);
-                if (!favoriteMovies.includes(movie._id)) {
-                    setFavorite(true);
-                    await fetch("https://myflix-movies.onrender.com/users/"+user.Username+"/movies/"+movie._id, {
-                        method: "POST",
-                        headers: {
-                          Authorization: `Bearer ${token}`,
-                          "Content-Type": "application/json"
-                        }
-                      }).then((response) => {
-                        addToFavoriteSuccess();
-                        return response.json();
-                      }).then(data => console.log(data))
-                      .catch(err => {
-                          alert("Something went wrong");
-                          console.log(err);
-                      });
-                } else {
-                    setFavorite(false);
-                    await fetch("https://myflix-movies.onrender.com/users/"+user.Username+"/movies/"+movie._id, {
-                        method: "DELETE",
-                        headers: {
-                          Authorization: `Bearer ${token}`,
-                          "Content-Type": "application/json"
-                        }
-                      }).then((response) => {
-                        removeFromFavoriteSuccess();
-                        return response.json();
-                      }).then(data => console.log(data))
-                      .catch(err => {
-                          alert("Something went wrong");
-                          console.log(err);
-                      });
-
-                }
+  const handleFavorite = async () => {
+    const user = JSON.parse(localStorage.getItem("user"));
+    const response = await fetch(
+      "https://myflix-movies.onrender.com/users/" + user.Username,
+      {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const data = await response.json();
+    const favoriteMovies = data.FavoriteMovies;
+    console.log(favoriteMovies);
+    if (!favoriteMovies.includes(movie._id)) {
+      setFavorite(true);
+      await fetch(
+        "https://myflix-movies.onrender.com/users/" +
+          user.Username +
+          "/movies/" +
+          movie._id,
+        {
+          method: "POST",
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+          },
+        }
+      )
+        .then((response) => {
+          addToFavoriteSuccess();
+          return response.json();
+        })
+        .then((data) => console.log(data))
+        .catch((err) => {
+          alert("Something went wrong");
+          console.log(err);
+        });
+    } else {
+      setFavorite(false);
+      await fetch(
+        "https://myflix-movies.onrender.com/users/" +
+          user.Username +
+          "/movies/" +
+          movie._id,
+        {
+          method: "DELETE",
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+          },
+        }
+      )
+        .then((response) => {
+          removeFromFavoriteSuccess();
+          return response.json();
+        })
+        .then((data) => console.log(data))
+        .catch((err) => {
+          alert("Something went wrong");
+          console.log(err);
+        });
     }
+  };
 
+  if (!movie) {
+    return <SpinnerComponent />;
+  }
+  return (
+    <Container>
+      <Card className="card-container--mainview">
+        <Row>
+          <Col xs={12} lg={6} md={9}>
+            <CardImg
+              className="card-image-movie-view"
+              src={movie.ImagePath}
+              alt="movie-poster"
+            />
+          </Col>
+          <Col xs={12} lg={6} md={9}>
+            <Card.Body className="card-body">
+              <Card.Title className="card-title-movie-view">
+                {movie.Title} ({movie.ReleaseYear})
+                <img
+                  src={favorite ? favorited : notFavorited}
+                  alt="favorite-icon"
+                  className="icon-favorite"
+                  title="Add to favorite"
+                  onClick={handleFavorite}
+                />
+              </Card.Title>
 
-    if (!movie) {
-        return <div>Loading movie data...</div>
-    }
-    return (
-            <Container>
-                    <Card className="card-container--mainview">
-                    <Row>
-                        <Col xs={12} lg={6} md={9}>
-                            <CardImg className="card-image-movie-view" src={movie.ImagePath} alt="movie-poster"/>
-                        </Col>
-                        <Col xs={12} lg={6} md={9}> 
-                            <Card.Body className="card-body">
-                                <Card.Title className="card-title-movie-view">{movie.Title} ({movie.ReleaseYear}) 
-                                <img src={favorite ? favorited : notFavorited} alt="favorite-icon" className="icon-favorite"  title="Add to favorite" onClick={handleFavorite}/>
-                                </Card.Title>
-                                
-                                <br></br>
-                                <Card.Text><span>Genre: </span> 
-                                <Link className="link-text" to={`/genre/${encodeURIComponent(movie.Genre.Name)}`}>
-                                {movie.Genre.Name} 
-                                </Link>
-                                </Card.Text>
-                                <Card.Text><span>Director: </span>
-                                <Link className="link-text" to={`/directors/${encodeURIComponent(movie.Director.Name)}`}>
-                                  {movie.Director.Name}
-                                </Link> 
-                                </Card.Text>
-                                <Card.Text><span>Summary: </span>{movie.Description} </Card.Text>
-                                <Button variant='outline-dark' className='back-btn mt-2' onClick={() => navigate("/")}>Back</Button>
-                            </Card.Body>
-                        </Col>
-                        </Row>
-                    </Card>
-
-            </Container>
-    )
-}
-
+              <br></br>
+              <Card.Text>
+                <span>Genre: </span>
+                <Link
+                  className="link-text"
+                  to={`/genre/${encodeURIComponent(movie.Genre.Name)}`}
+                >
+                  {movie.Genre.Name}
+                </Link>
+              </Card.Text>
+              <Card.Text>
+                <span>Director: </span>
+                <Link
+                  className="link-text"
+                  to={`/directors/${encodeURIComponent(movie.Director.Name)}`}
+                >
+                  {movie.Director.Name}
+                </Link>
+              </Card.Text>
+              <Card.Text>
+                <span>Summary: </span>
+                {movie.Description}{" "}
+              </Card.Text>
+              <Button
+                variant="outline-dark"
+                className="back-btn mt-2"
+                onClick={() => navigate("/")}
+              >
+                Back
+              </Button>
+            </Card.Body>
+          </Col>
+        </Row>
+      </Card>
+    </Container>
+  );
+};
 
 MovieView.propTypes = {
-    movieData: PropTypes.array.isRequired
-  };
+  movieData: PropTypes.array.isRequired,
+};
